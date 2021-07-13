879a021c1b3d31aa90fd12da769297638c225e89
berryberry189 <berryberry189@naver.com>
2021-07-13 17:02:46 +0900

    lend 작업 파일 생성

---
+++ src/main/java/com/jpa/lendlocker/repository/LendRepository.java
@@ -0,0 +1,7 @@
+package com.jpa.lendlocker.repository;
+
+import com.jpa.lendlocker.entity.Lend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LendRepository extends JpaRepository<Lend, Long> {
+}
