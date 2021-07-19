b34eff01951b67bae63066422493a5337bc5ee8e
berryberry189 <berryberry189@naver.com>
2021-07-19 17:39:07 +0900

    id -> userKey로 변경

--- src/main/java/com/jpa/lendlocker/entity/User.java
+++ src/main/java/com/jpa/lendlocker/entity/User.java
@@ -18,7 +18,7 @@ public class User{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_key")
-    private Long id;
+    private Long userKey;
 
     @Column(name = "user_id", nullable = false)
     private String userId;
