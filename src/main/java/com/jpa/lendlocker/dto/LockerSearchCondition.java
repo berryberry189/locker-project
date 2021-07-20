e127cae912e57c9276acfc30ca8acbfbb2afbeea
berryberry189 <berryberry189@naver.com>
2021-07-20 21:20:07 +0900

    LockerSearchCondition 생성

---
+++ src/main/java/com/jpa/lendlocker/dto/LockerSearchCondition.java
@@ -0,0 +1,21 @@
+package com.jpa.lendlocker.dto;
+
+import com.jpa.lendlocker.entity.LockerId;
+import com.jpa.lendlocker.enums.LockerType;
+import io.swagger.annotations.ApiParam;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LockerSearchCondition {
+
+    @ApiParam(value = "[검색 조건] 사물함 구역& 번호")
+    private LockerId lockerId;
+
+    @ApiParam(value = "[검색 조건] 사물함 종류")
+    private LockerType type;
+
+    @ApiParam(value = "[검색 조건] 사용 여부")
+    private String useYn;
+
+}
