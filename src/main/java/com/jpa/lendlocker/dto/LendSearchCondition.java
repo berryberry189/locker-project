b17b9f8888ee04a3f83f0a8da54ab7bf17483d48
berryberry189 <berryberry189@naver.com>
2021-07-21 00:14:30 +0900

    LendSearchCondition 생성

---
+++ src/main/java/com/jpa/lendlocker/dto/LendSearchCondition.java
@@ -0,0 +1,31 @@
+package com.jpa.lendlocker.dto;
+
+import com.jpa.lendlocker.enums.LendStatus;
+import io.swagger.annotations.ApiParam;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class LendSearchCondition {
+
+    @ApiParam(value = "[검색 조건] 사용자 ID")
+    private String userId;
+
+    @ApiParam(value = "[검색 조건] 구역 명")
+    private String areaName;
+
+    @ApiParam(value = "[검색 조건] 대여 상태")
+    private LendStatus status;
+
+    @ApiParam(value = "[검색 조건] 기간 검색 시작 날짜")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDateTime;
+
+    @ApiParam(value = "[검색 조건] 기간 검색 종료 날짜")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime endDateTime;
+
+}
