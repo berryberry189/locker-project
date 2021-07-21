6214e55cab42155a5cdb62dd3e1ded5ec67e0178
berryberry189 <berryberry189@naver.com>
2021-07-21 16:47:12 +0900

    findByAreaId orderBy 추가

--- src/main/java/com/jpa/lendlocker/repository/LockerRepository.java
+++ src/main/java/com/jpa/lendlocker/repository/LockerRepository.java
@@ -3,9 +3,12 @@ package com.jpa.lendlocker.repository;
 import com.jpa.lendlocker.entity.Locker;
 import com.jpa.lendlocker.entity.LockerId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface LockerRepository extends JpaRepository<Locker, LockerId>, LockerRepositoryCustom {
+
+    @Query("select l from Locker l where l.area.id = :areaId order by l.lockerId.lockerNo")
     List<Locker> findByAreaId(Long areaId);
 }
