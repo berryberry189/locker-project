a455923f80c1966d184191c2d2879bdfb89acf44
berryberry189 <berryberry189@naver.com>
2021-07-24 18:42:23 +0900

    사용자의 대여 list 필드 추가

--- src/main/java/com/jpa/lendlocker/dto/UserResponseDto.java
+++ src/main/java/com/jpa/lendlocker/dto/UserResponseDto.java
@@ -1,9 +1,12 @@
 package com.jpa.lendlocker.dto;
 
+import com.jpa.lendlocker.entity.Lend;
 import com.jpa.lendlocker.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @NoArgsConstructor
 @Getter @Setter
@@ -15,12 +18,20 @@ public class UserResponseDto {
     private String mobile;
     // 사용자별 라커 갯수
     private Long lockerNum;
+    private List<UserLendResponseDto> lends;
 
     public UserResponseDto(User user) {
         this.userKey = user.getUserKey();
         this.userId = user.getUserId();
         this.name = user.getName();
         this.mobile = user.getMobile();
+    }
 
+    public UserResponseDto(Long userKey, String userId, String name, String mobile, List<UserLendResponseDto> lends) {
+        this.userKey = userKey;
+        this.userId = userId;
+        this.name = name;
+        this.mobile = mobile;
+        this.lends = lends;
     }
 }
