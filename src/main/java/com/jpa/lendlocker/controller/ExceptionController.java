8d6819a59c2fc8d84a4d7874fadc671bb1a791c8
berryberry189 <berryberry189@naver.com>
2021-08-06 23:33:24 +0900

    코드 리펙토링 - 예외처리 예외메시지 body로 전달

--- src/main/java/com/jpa/lendlocker/controller/ExceptionController.java
+++ src/main/java/com/jpa/lendlocker/controller/ExceptionController.java
@@ -3,8 +3,8 @@ package com.jpa.lendlocker.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -12,28 +12,28 @@ import javax.persistence.EntityNotFoundException;
  * 공통 예외처리 클래스
  */
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionController {
 
     /**
      * PK로 조회 시 결과 값이 없을 경우 응답코드 404 반환
-     * @param exception
+     * @param e
      * @return
      */
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity entityNotFoundException(final RuntimeException exception) {
-        log.info("error", exception);
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    public ResponseEntity entityNotFoundException(EntityNotFoundException e) {
+        log.info("error", e);
+        return new ResponseEntity(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     /**
      * 클라이언트 입력 값이 유효하지 않을 경우 응답코드 400 반환
-     * @param exception
+     * @param e
      * @return
      */
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity illegalArgumentException(final RuntimeException exception) {
-        log.info("error", exception);
-        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    public ResponseEntity illegalArgumentException(IllegalArgumentException e) {
+        log.info("error", e);
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
