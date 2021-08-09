cdc96bc858df6536e7ee01a6eab41cdbaf08a4f1
berryberry189 <berryberry189@naver.com>
2021-08-09 20:50:13 +0900

    페이징 파라미터 추가

--- src/main/java/com/jpa/lendlocker/config/SwaggerConfig.java
+++ src/main/java/com/jpa/lendlocker/config/SwaggerConfig.java
@@ -1,10 +1,18 @@
 package com.jpa.lendlocker.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -12,7 +20,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final TypeResolver typeResolver;
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Lend Locker")
@@ -24,10 +36,23 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(this.apiInfo())
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class)))
                 .select()
                 .apis(RequestHandlerSelectors
-                        .basePackage("com.jpa.lendlocker.controller"))
+                        .basePackage("com.jpa.offer.controller"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Getter
+    @Setter
+    @ApiModel
+    static class Page {
+        @ApiModelProperty(value = "페이지 번호(0..N)")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
+        private Integer size;
     }
 }
